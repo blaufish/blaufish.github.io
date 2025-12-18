f93a4efe9ca08e942d9e442a26b7931b90f60d51
blaufish <blaufish@users.noreply.github.com>
2025-12-18 16:33:04 +0100

    Support spellchecking a single file

--- spellcheck.sh
+++ spellcheck.sh
@@ -1,9 +1,21 @@
 #!/bin/bash
 
 ASPELL_DIR="$PWD/.aspell"
+
+spellcheck() {
+	local file="$1"
+	aspell check --mode=markdown --lang=en --home-dir="$ASPELL_DIR" -- "$file"
+}
+
 mkdir -p -- "$ASPELL_DIR"
+
+if [[ "$1" != "" ]]
+then
+	spellcheck "$1"
+	exit
+fi
 
 for file in $(find docs -name "*.md" -type f)
 do
-    aspell check --mode=markdown --lang=en --home-dir="$ASPELL_DIR" -- "$file"
+	spellcheck "$file"
 done
